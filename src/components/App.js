aac90cecf9a17fb7a1e65dfe3298d3e1f392bb4e
jorgadev <radovanjorgic14@gmail.com>
2021-02-23 06:45:48 +0100

    React router added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../assets/css/Styles.scss";
 
 import Navbar from "./Navbar";
 import Subnavbar from "./Subnavbar";
+import Landing from "./Landing";
 
 function App() {
   return (
     <>
       <Navbar />
       <Subnavbar />
+      <Router>
+        <Route exact path="/" component={Landing} />
+        {/* <PrivateRoute path="/dashboard" component={} />
+        <PrivateRoute path="/teams" component={} />
+        <PrivateRoute path="/projects" component={} />
+        <PrivateRoute path="/settings" component={} />
+        <Route path="/signup" component={} />
+        <Route path="/login" component={} />
+        <Route path="/forgot-password" component={} /> */}
+      </Router>
     </>
   );
 }
